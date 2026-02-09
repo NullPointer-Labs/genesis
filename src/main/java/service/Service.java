340a2bc5794c4da5661573385480744f772ad1c1
Wesley00s <wesley300rodrigues@gmail.com>
2026-02-08 22:30:00 -0300

    refactor: Update Service to use RouteScanner for controller registration

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -1,5 +1,10 @@
 package service;
 
+import controllers.UserController;
+import http.Dispatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,61 +12,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Service {
+    private static final Logger logger = LoggerFactory.getLogger(Service.class);
     public void execute(int port) {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server running in port " + port);
+            System.out.println("🚀 Server running in port " + port);
 
-            Dispatcher dispatcher = registerControllers();
-            ExecutorService theadPool = Executors.newFixedThreadPool(50);
+            Dispatcher dispatcher = new Dispatcher();
+
+            RouteScanner.scan(new UserController(), dispatcher);
+
+            ExecutorService threadPool = Executors.newFixedThreadPool(50);
 
             while (!serverSocket.isClosed()) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("New Connection: " + clientSocket.getInetAddress());
-
                 ClientHandler handler = new ClientHandler(clientSocket, dispatcher);
-                theadPool.execute(handler);
+                threadPool.execute(handler);
             }
         } catch (IOException e) {
-            System.out.println("Fatal server error: " + e.getMessage());
+            logger.error("Service Error: {}",  e.getMessage());
         }
-    }
-
-    private static Dispatcher registerControllers() {
-        Dispatcher dispatcher = new Dispatcher();
-        dispatcher.register("/api", (req, res) -> {
-            res.setStatusCode(200);
-            res.setBody("{\"status\": \"api working\"}");
-            res.setContentType("application/json");
-        });
-
-        dispatcher.register("/api/user", (req, res) -> {
-            if (req.getMethod().equals("GET")) {
-                res.setStatusCode(200);
-                res.setBody("{\"id\": 1, \"name\": \"Wesley\", \"role\": \"Java Developer\"}");
-                res.setContentType("application/json");
-
-            } else if (req.getMethod().equals("POST")) {
-                String body = req.getBody();
-                res.setStatusCode(201);
-                res.setBody("{\"message\": \"User created\", \"data\": " + body + "}");
-                res.setContentType("application/json");
-            }
-        });
-
-        dispatcher.register("/api/search", (req, res) -> {
-            String query = req.getParam("q");
-
-            if (query == null) {
-                res.setStatusCode(400);
-                res.setBody("{\"error\": \"Missing query param 'q'\"}");
-                res.setContentType("application/json");
-                return;
-            }
-
-            res.setStatusCode(200);
-            res.setBody("{\"result\": \"Searching for: " + query + "\"}");
-            res.setContentType("application/json");
-        });
-        return dispatcher;
     }
 }
