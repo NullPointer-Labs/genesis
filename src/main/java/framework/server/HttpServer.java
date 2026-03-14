6bcb86499fc2dd7e18a3c99182497fc4c9874cf8
Wesley00s <wesley300rodrigues@gmail.com>
2026-03-14 15:17:43 -0300

    refactor: Switched to virtual threads in HttpServer. Replaced fixed thread pool.

--- src/main/java/framework/server/HttpServer.java
+++ src/main/java/framework/server/HttpServer.java
@@ -37,7 +37,7 @@ public class HttpServer {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server running in port " + port);
 
-            ExecutorService threadPool = Executors.newFixedThreadPool(50);
+            ExecutorService threadPool = Executors.newVirtualThreadPerTaskExecutor();
 
             while (!serverSocket.isClosed()) {
                 Socket clientSocket = serverSocket.accept();
